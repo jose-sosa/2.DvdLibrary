d1021f94386122ee0eff71ff9230d7283a72c398
Jose Sosa <jys5219@gmail.com>
2018-06-21 20:53:11 -0400

    creating repo

---
+++ src/main/java/com/sg/dvdlibrary/dao/DvdLibraryDao.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.dvdlibrary.dao;
+
+import com.sg.dvdlibrary.dto.Dvd;
+import java.util.List;
+
+/**
+ *
+ * @author josesosa
+ */
+public interface DvdLibraryDao {
+    
+	    
+    List<Dvd> getAllDvds() throws DvdLibraryDaoException;
+    
+    Dvd addDvd(String dvdTitle, Dvd dvd) throws DvdLibraryDaoException;
+
+    Dvd removeDvd(String dvdTitle) throws DvdLibraryDaoException;
+    
+    Dvd getDvd(String dvdTitle) throws DvdLibraryDaoException;
+    
+}
+
+
+//// 4 edit    5 search and view
